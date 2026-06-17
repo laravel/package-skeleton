5c0e78d8de56d0d25200eb7020cc511acaaaf012
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-06-17 15:08:23 -0400

    Update skeleton.php

--- config/skeleton.php
+++ config/skeleton.php
@@ -9,9 +9,8 @@ return [
     | Placeholder Value
     |--------------------------------------------------------------------------
     |
-    | This value is a packaged default that demonstrates how host applications
-    | can override the configuration after merging. Replace this block when
-    | you evolve the skeleton into your own package.
+    | This value is a packaged default that your application can override
+    | after publishing. Replace this block with your own package options.
     |
     */
 
