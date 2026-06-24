29addfc113534d0b691242d9a4c4a3e6dab6cfd7
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-06-24 14:45:54 -0400

    Update SkeletonServiceProvider.php

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace VendorName\Skeleton;
 
-use Closure;
 use Illuminate\Support\ServiceProvider;
 /* @chisel-commands */
 use VendorName\Skeleton\Console\Commands\SkeletonCommand;
@@ -30,57 +29,56 @@ class SkeletonServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        /* @chisel-config */
-        $this->whenInConsole(fn () => $this->publishes([
-            __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
-        ], ['skeleton', 'skeleton-config']));
-        /* @end-chisel-config */
-
         /* @chisel-routes */
         $this->loadRoutesFrom(__DIR__.'/../routes/skeleton.php');
         /* @end-chisel-routes */
 
         /* @chisel-views */
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
-
-        $this->whenInConsole(fn () => $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/skeleton'),
-        ], ['skeleton', 'skeleton-views']));
         /* @end-chisel-views */
 
         /* @chisel-translations */
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'skeleton');
-
-        $this->whenInConsole(fn () => $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/skeleton'),
-        ], ['skeleton', 'skeleton-lang']));
         /* @end-chisel-translations */
 
-        /* @chisel-migrations */
-        $this->whenInConsole(fn () => $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], ['skeleton', 'skeleton-migrations']));
-        /* @end-chisel-migrations */
-
-        /* @chisel-assets */
-        $this->whenInConsole(fn () => $this->publishes([
-            __DIR__.'/../public' => public_path('vendor/skeleton'),
-        ], ['skeleton', 'skeleton-assets']));
-        /* @end-chisel-assets */
-
-        /* @chisel-commands */
-        $this->whenInConsole(fn () => $this->commands([
-            SkeletonCommand::class,
-        ]));
-        /* @end-chisel-commands */
-    }
-
-    private function whenInConsole(Closure $callback): void
-    {
         if (! $this->app->runningInConsole()) {
             return;
         }
 
-        $callback();
+        /* @chisel-config */
+        $this->publishes([
+            __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+        ], ['skeleton', 'skeleton-config']);
+        /* @end-chisel-config */
+
+        /* @chisel-views */
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/skeleton'),
+        ], ['skeleton', 'skeleton-views']);
+        /* @end-chisel-views */
+
+        /* @chisel-translations */
+        $this->publishes([
+            __DIR__.'/../lang' => $this->app->langPath('vendor/skeleton'),
+        ], ['skeleton', 'skeleton-lang']);
+        /* @end-chisel-translations */
+
+        /* @chisel-migrations */
+        $this->publishesMigrations([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], ['skeleton', 'skeleton-migrations']);
+        /* @end-chisel-migrations */
+
+        /* @chisel-assets */
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/skeleton'),
+        ], ['skeleton', 'skeleton-assets']);
+        /* @end-chisel-assets */
+
+        /* @chisel-commands */
+        $this->commands([
+            SkeletonCommand::class,
+        ]);
+        /* @end-chisel-commands */
     }
 }
