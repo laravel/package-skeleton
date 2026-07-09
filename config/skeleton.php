31927f4198ffb7b1be0ba173549b48cc9637913b
Taylor Otwell <taylor@laravel.com>
2026-07-09 07:51:30 -0500

    Clean up placeholder value comments in skeleton.php
    Removed placeholder value documentation comments.

--- config/skeleton.php
+++ config/skeleton.php
@@ -4,16 +4,6 @@ declare(strict_types=1);
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Placeholder Value
-    |--------------------------------------------------------------------------
-    |
-    | This value is a packaged default that your application can override
-    | after publishing. Replace this block with your own package options.
-    |
-    */
-
     'placeholder' => 'default',
 
 ];
